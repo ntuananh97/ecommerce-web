1c1fb804526adfb21987e17c8d628a726b154e88
LAPTOP-UMM7HUUJ\user <ntuananh2897@gmail.com>
2024-03-20 22:33:35 +0700

    install necessary packages

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const compression = require('compression');
+const express = require('express');
+const { default: helmet } = require('helmet');
+const morgan = require('morgan'); // logger when request comes in
+
+const app = express();
+
+// init middleware
+
+app.use(morgan('dev')); // use in development
+// app.use(morgan('combined')); // use in production
+
+app.use(helmet()); // secure app by setting various HTTP headers
+app.use(compression()); // reduce the size of the response body and increase the speed of a web app
+
+
+// init db
+
+// init routes
+app.get('/', (req, res, next) => { res.send('Hello, world!'); });
+
+// handle errors
+
+module.exports = app;
